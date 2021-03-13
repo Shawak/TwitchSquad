d8492ac0ec73ba4f4848b086d46df88f8780d59e
Maximilian <Shawak@users.noreply.github.com>
2021-03-13 19:48:45 +0100

    Update twitchsquad.user.js

--- twitchsquad.user.js
+++ twitchsquad.user.js
@@ -22,30 +22,6 @@ if (typeof GM_addStyle === 'undefined') {
 }
 
 GM_addStyle(`
-// ==UserScript==
-// @name         TwitchSquad
-// @namespace    https://github.com/Shawak/TwitchSquad
-// @author       Shawak
-// @version      0.0.1
-// @description  Show twitch squad streams side by side.
-// @match        *://*.twitch.tv/*
-// @downloadURL  https://github.com/Shawak/TwitchSquad/raw/master/twitchsquad.user.js
-// @updateURL    https://github.com/Shawak/TwitchSquad/raw/master/twitchsquad.user.js
-// @grant        GM_addStyle
-// ==/UserScript==
-
-if (typeof GM_addStyle === 'undefined') {
-  GM_addStyle = function(css) {
-    let head = document.getElementsByTagName('head')[0], style = document.createElement('style');
-    if (!head) {return}
-    style.type = 'text/css';
-    try {style.innerHTML = css}
-    catch(x) {style.innerText = css}
-    head.appendChild(style);
-  };
-}
-
-GM_addStyle(`
 .multi-stream-player-layout__player-container {
     display: flex;
 }
@@ -56,6 +32,4 @@ GM_addStyle(`
     width: 50% !important;
     position: unset !important;
 }
-`);
-
 `);
